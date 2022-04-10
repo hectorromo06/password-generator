911851545d93936b023af09f98aa280286d14327
Hector Romo <hectorommmo@gmail.com>
2022-04-09 22:08:06 -0700

    declared character arrays

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,9 @@
 // Assignment code here
-
+// Will pull characters from the following arrays
+var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "-", ".", "`", "~", "|", "<", ">", "=", "_"];
+var numeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
